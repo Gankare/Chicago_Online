a0d3ee8e975055bb2c14615ae67a5374a8952397
Gankare <filipsalomonsson01@gmail.com>
2024-01-31 23:41:31 +0100

    LoadingScreen added

---
+++ Chicago_Online/Assets/Scripts/Menus/LoadingScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    private float timer;
+    public Slider loadingBar;
+
+    void Start()
+    {
+        timer = 0;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        loadingBar.value = timer;
+        if (timer > 1)
+            Destroy(gameObject);
+    }
+}
